fa209c602939f68a5425f47dd3021940ff131554
Alisson <alissondedeussiqueira@gmail.com>
2025-09-27 19:27:18 +0000

    diminua um pouquinho mais

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -101,7 +101,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        jump: 'jump 2s ease-in-out infinite',
+        jump: 'jump 3s ease-in-out infinite',
       },
     },
   },
