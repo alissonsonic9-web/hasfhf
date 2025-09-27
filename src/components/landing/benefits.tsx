992c8c5eb5442f2e6376ada818caf717777adb9c
Alisson <alissondedeussiqueira@gmail.com>
2025-09-27 19:31:19 +0000

    um pouco mais para cima na versão mobile

--- src/components/landing/benefits.tsx
+++ src/components/landing/benefits.tsx
@@ -128,7 +128,7 @@ export function Benefits() {
             ))}
           </div>
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center -mt-8 sm:mt-0">
           <a href="#pricing">
             <Button
               size="lg"
