d7fb94fc068b2c80b349fb5f9ea4fe8a88851896
Alisson <alissondedeussiqueira@gmail.com>
2025-09-27 19:14:38 +0000

    deixe o fundo dessa sessão na cor azul escuro o mesmo da sessão de preço

--- src/components/landing/modules.tsx
+++ src/components/landing/modules.tsx
@@ -30,7 +30,7 @@ const benefits = [
 
 export function Modules() {
   return (
-    <section className="w-full py-12 md:py-24 lg:py-32 bg-primary text-primary-foreground">
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-blue-900 text-primary-foreground">
       <div className="container px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">Com esse <span className="text-orange-500">MATERIAL</span> você terá!</h2>
