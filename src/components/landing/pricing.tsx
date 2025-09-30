f2f99cb17cc78a2cb179c06beb84bd928e159a0e
Alisson <alissondedeussiqueira@gmail.com>
2025-09-30 00:08:13 +0000

    aparentemente nn deu certo, macho vou te mandar aq a sessão completa do

--- src/components/landing/pricing.tsx
+++ src/components/landing/pricing.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link';
 import {
   Card,
   CardContent,
-  CardHeader,
   CardFooter,
+  CardHeader,
   CardTitle,
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
